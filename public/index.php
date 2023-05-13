0dec324775e79c60fbc5534e5d00461ce0b8a100
Zaher Ghaibeh <zaher@zaher.dev>
2023-05-13 13:09:56 +0300

    initial work for the template

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+define('WP_USE_THEMES', true);
+require dirname(__FILE__) . '/core/wp-blog-header.php';
---
+++ support/helpers.php
@@ -0,0 +1,42 @@
+<?php
+
+use PhpOption\Option;
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     * This is a modified version of
+     * https://github.com/laravel/framework/blob/a7f454c4d4b84fd7a1e18f77cfbc38d032481936/src/Illuminate/Support/Env.php#L66-L100
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env(string $key, mixed $default = null): mixed
+    {
+        return Option::fromValue($_ENV[$key] ?? $default)
+            ->map(function ($value) {
+                switch (strtolower($value)) {
+                    case 'true':
+                    case '(true)':
+                        return true;
+                    case 'false':
+                    case '(false)':
+                        return false;
+                    case 'empty':
+                    case '(empty)':
+                        return '';
+                    case 'null':
+                    case '(null)':
+                        return;
+                }
+
+                if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+                    return $matches[2];
+                }
+
+                return $value;
+            })
+            ->getOrCall(fn () => $default);
+    }
+}
